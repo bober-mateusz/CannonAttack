980c5848d176e871269eb7086f08bd388e9ea014
bober-mateusz <bober.mateusz17@gmail.com>
2020-09-25 20:04:30 +0100

    Setup Testing Environment

---
+++ CannonAttackTest/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CannonAttackTest
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void CannonIDValid()
+        {
+            Cannon cannon = new Cannon();
+            Assert.IsNotNull(cannon.ID);
+        }
+    }
+}
+
