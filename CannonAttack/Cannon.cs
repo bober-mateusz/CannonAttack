90b11e44e43996b6f54a498810b5a27c093272af
bober-mateusz <bober.mateusz17@gmail.com>
2020-09-28 00:40:05 +0100

    final committ

--- CannonAttack/Cannon.cs
+++ CannonAttack/Cannon.cs
@@ -13,7 +13,30 @@ namespace CannonAttack
         private string CannonID;
         public static readonly int MAXANGLE = 90;
         public static readonly int MINANGLE = 1;
+        private readonly int MAXVELOCITY = 30000000;
+        private readonly int MAXDISTANCEOFTARGET = 20000;
+        private int distanceOfTarget;
+        private readonly double GRAVITY = 9.8;
+        private readonly int BURSTRADIUS = 50;
+        private int shots;
 
+
+        public void Reset()
+        {
+            shots = 0;
+        }
+        public int DistanceOfTarget
+        {
+            get { return distanceOfTarget; }
+            set { distanceOfTarget = value; }
+        }
+        public int Shots
+        {
+            get
+            {
+                return shots;
+            }
+        }
         public string ID
         {
             get
@@ -31,7 +54,8 @@ namespace CannonAttack
         static readonly object padlock = new object();
         private Cannon()
         {
-
+            Random r = new Random();
+            SetTarget(r.Next(MAXDISTANCEOFTARGET));
         }
 
         public static Cannon GetInstance()
@@ -51,11 +75,54 @@ namespace CannonAttack
         {
             if (angle > MAXANGLE || angle < MINANGLE)
             {
-                return Tuple.Create(false, "Angle Incoorect");
+                return Tuple.Create(false, "Angle Incorrect");
             }
-            return Tuple.Create(true, "Angle OK");
-            
+            if (velocity > MAXVELOCITY)
+            {
+                return Tuple.Create(false, "Velocity of the cannon cannot travel faster than the speed of light");
+            }
+            string message;
+            bool hit;
+            int distanceOfShot = CalculateDistanceOfCannonShot(angle, velocity);
+            if (distanceOfShot.WithinRange(this.distanceOfTarget, BURSTRADIUS))
+            {
+                message = String.Format("Hit - {0} Shot(s)",shots);
+                hit = true;
+            }
+            else
+            {
+                message = String.Format("Missed Cannonball landed at {0} Meters", distanceOfShot);
+                hit = false;
+            }
+            shots++;
+            return Tuple.Create(hit, message);
         }
 
+        public int CalculateDistanceOfCannonShot(int angle, int velocity)
+        {
+            int time = 0;
+            double height = 0;
+            double distance = 0;
+            double angleInRadians = (3.1415926536 / 180) * angle;
+            while (height >= 0)
+            {
+                time++;
+                distance = velocity * Math.Cos(angleInRadians) * time;
+                height = (velocity * Math.Sin(angleInRadians) * time) - (GRAVITY * Math.Pow(time, 2)) / 2;
+            }
+            return (int)distance;
+        }
+
+        public void SetTarget(int distanceOfTarget)
+        {
+            if(!distanceOfTarget.Between(0, MAXDISTANCEOFTARGET))
+            {
+                throw new ApplicationException(String.Format("Target distance must be between 1 and {0} meters", MAXDISTANCEOFTARGET));
+            }
+            this.distanceOfTarget = distanceOfTarget;
+        }
+
+
+       
     }
 }
--- CannonAttackTest/TestClass.cs
+++ CannonAttackTest/TestClass.cs
@@ -7,7 +7,13 @@ using NUnit.Framework;
 using CannonAttack;
 namespace CannonAttackTest
 {
-    
+    [TestFixtureSetUp]
+    public class inital
+    {
+       
+    }
+
+
 
     [TestFixture]
     public class TestClass
@@ -34,6 +40,43 @@ namespace CannonAttackTest
             var shot = cannon.shoot(95, 100);
             Assert.IsFalse(shot.Item1);
         }
+
+        [Test]
+        public void TestCannonShootVelocityGreaterThanSpeedOfLight()
+        {
+            Cannon cannon = Cannon.GetInstance();
+            var shot = cannon.shoot(45, 300000001);
+            Assert.IsFalse(shot.Item1);
+        }
+
+        [Test]
+        public void TestCannonShootMiss()
+        {
+            Cannon cannon = Cannon.GetInstance();
+            cannon.SetTarget(2000);
+            var shot = cannon.shoot(45, 350);
+            Assert.IsTrue(shot.Item2 == "Missed Cannonball landed at 12621 Meters");
+        }
+
+        [Test]
+        public void TestCannonShootHit()
+        {
+            Cannon cannon = Cannon.GetInstance();
+            cannon.SetTarget(12621);
+            var shot = cannon.shoot(45, 350);
+            Assert.IsTrue(shot.Item2 == "Hit");
+        }
+
+        [Test]
+        public void TestCannonCountShots()
+        {
+            Cannon cannon = Cannon.GetInstance();
+            cannon.SetTarget(12621);
+            var shot = cannon.shoot(45, 350);
+            Assert.IsTrue(shot.Item2 == "Hit - 1 Shot(s)", "Number of shots :" + cannon.Shots);
+        }
+
+
     }
 
 
